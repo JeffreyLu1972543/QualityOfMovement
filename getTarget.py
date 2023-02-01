100303a9e0594592e31af7ce6e8265faad6a963a
sh1670 <sh1670@bristol.ac.uk>
2023-02-01 15:29:52 +0000

    tweaks

--- getTarget.py
+++ getTarget.py
@@ -2,23 +2,27 @@ import os
 import json
 import numpy as np
 
+# root_vid_details = '/Users/jeffreylu/Desktop/jk_data_only_22/video_details'
+root_vid_details = 'F:/WORK/DATASETS/jk_data_only_22/video_details'
+
 def flatten(l):
     return [item for sublist in l for item in sublist]
 # return a list of file path of all json files in video_detail folder
 def get_json_path(dirname):
+
     jsonfiles_path=[]
     for dirpath,dirnames,filenames in os.walk(dirname):
         print(len(filenames)) # get all filenames in video_detail
     for i in range(len(filenames)):
-        jsonfile=os.path.join('/Users/jeffreylu/Desktop/jk_data_only_22/video_details',filenames[i]) # get path of all files in video_detail
+        jsonfile=os.path.join(root_vid_details, filenames[i]) # get path of all files in video_detail
         jsonfiles_path.append(jsonfile)
     return jsonfiles_path
 
 def get_targetValues(jsonfiles_path):
     targetClass=[]
     length=len(jsonfiles_path)
-    for j in range(13):#should be for j in range(length)
-        # print(j)
+    for j in range(length):#should be for j in range(length)
+        print(jsonfiles_path[j])
         with open(jsonfiles_path[j], 'r') as f:
             data = json.load(f)
             # print("hi")
@@ -28,7 +32,7 @@ def get_targetValues(jsonfiles_path):
     return targetClass_list
 
 
-jsonfiles_path=get_json_path('/Users/jeffreylu/Desktop/jk_data_only_22/video_details')
+jsonfiles_path=get_json_path(root_vid_details)
 targetClass_list=get_targetValues(jsonfiles_path)
 print(len(targetClass_list))
 print(targetClass_list)
